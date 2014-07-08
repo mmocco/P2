a5f2a377dfe7da2d1a96e16cdf7a72fe06d14bb9
mmocco <mmocco@college.harvard.edu>
2014-07-08 12:52:52 -0400

    Adding css and arrays

--- passbrain.php
+++ passbrain.php
@@ -1,19 +1,18 @@
 <?php
 
 
-	$bank = array ("There", "is", "a", "house", "in", "New Orleans", "it's", "called", "the", "rising", "sun", "ruin", "millions", "poor", "boy", "god", "I", "know", "one");
+
+	$bank = array ("there", "is", "a", "house", "in", "New Orleans", "it's", "called", "the", "rising", "sun", "ruin", "millions", "poor", "boy", "god", "I", "know", "one", "over", "kilarney", "many", "years", "ago", "my", "mother", "sang", "song", "tones", "sweet", "and", "low");
+
+
+
+// Debugging: Show the contents of the $_POST superglobal
+echo "This is a POST<br>";
+print_r($_POST);
+echo "<br>This is a Bank<br>";
+print_r($bank);
 
 
 
 	$pick = rand(0,20);
-#	$password = "";
-#	$picked = "";
-	$j = 0;
-	
-
-	$picked= $picked.$pick;
-	$password=$password.$array[$pick];
-
-	$pick = rand(0,20);
-	$j++;
 
--- passwordgen.php
+++ passwordgen.php
@@ -3,22 +3,24 @@
 <head>
 
 	<title>Password Generator</title>
-
+        <meta charset="utf-8">
         <link rel='stylesheet' href='passstyles.css' type='text/css'>
-	<?php require 'passbrain.php' ?> 
+	<?php require 'passbrain.php'; ?> 
 
 </head>
-<body>
-	<form form action ='passbrain.php' method= 'POST'>
+<body class= "center">
+	<form action ='passbrain.php' method= 'POST'>
 
 	<p>    </p>
 <!-- Landing page includes a description of your app and what a xkcd password is (assume an unfamiliar audience).
 		put input form here -->	
-        <input type = 'text' name = 'in1' ><br> How many words do you want in your password?
+        <input type = 'text' name = 'in1' ><br> How many words do you want in your password?<br>
 
-	<input type="checkbox" name="number" value="Bike">Should your password include a number?<br>
-	<input type="checkbox" name="symbol" value="Car">Should your password include symbols?<br> 
-	<input type="checkbox" name="symbol" value="Car">Should your passworf have an UPPER CASE first letter?
+	<input type="checkbox" name="number" value="false" class ="check">Should your password include a number?<br>
+	<input type="checkbox" name="symbol" value="false" class ="check">Should your password include symbols?<br> 
+	<input type="checkbox" name="case" value="false" class = "check">Should your passworf have an UPPER CASE first letter?<br>
+Generate! <input type="submit"">
+
 </form>
 
 	</form>
