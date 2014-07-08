f733af0a047e3cf7ed91b10cc4f2315a4b9beabc
mmocco <mmocco@college.harvard.edu>
2014-07-08 12:02:41 -0400

    first commit

---
+++ passbrain.php
@@ -0,0 +1,19 @@
+<?php
+
+
+	$bank = array ("There", "is", "a", "house", "in", "New Orleans", "it's", "called", "the", "rising", "sun", "ruin", "millions", "poor", "boy", "god", "I", "know", "one");
+
+
+
+	$pick = rand(0,20);
+	$password = "";
+	$picked = "";
+	$j = 0;
+	
+
+	$picked= $picked.$pick;
+	$password=$password.$array[$pick];
+
+	$pick = rand(0,20);
+	$j++;
+	}
---
+++ passwordgen.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Password Generator</title>
+
+        <link rel='stylesheet' href='passstyles.css' type='text/css'>
+	<?php require 'passbrain.php' ?> 
+
+</head>
+<body>
+	<form form action ='passbrain.php' method= 'POST'>
+		<!-- put input form here -->	
+        What goes in the form:   <input type = 'text' name = 'in1' ><br>
+	</form>
+
+	
+	
+
+</body>
+</html>
